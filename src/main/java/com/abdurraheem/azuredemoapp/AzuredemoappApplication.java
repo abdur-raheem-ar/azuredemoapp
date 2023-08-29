993a9f5d50bbba519e42f9dcb8dcd4f7abca7722
Abdur Raheem <raheemabdur1712@gmail.com>
2023-08-29 10:28:53 +0530

    first commit

---
+++ src/main/java/com/abdurraheem/azuredemoapp/AzuredemoappApplication.java
@@ -0,0 +1,13 @@
+package com.abdurraheem.azuredemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzuredemoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzuredemoappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abdurraheem/azuredemoapp/AzuredemoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdurraheem.azuredemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzuredemoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
